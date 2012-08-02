7dedccc2847b4e4f98610902da812f06eaefabce
Alberto Gutiérrez Martínez <albertogutim@MacBook-Air-de-Alberto.local>
2012-08-02 23:57:33 +0200

    Los que quiero quitar

---
+++ PFC-AttendanceTests/PFC_AttendanceTests.h
@@ -0,0 +1,13 @@
+//
+//  PFC_AttendanceTests.h
+//  PFC-AttendanceTests
+//
+//  Created by ANA GUTIÉRREZ ESGUEVILLAS GUTIERREZ ESGUEVILLAS on 31/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PFC_AttendanceTests : SenTestCase
+
+@end
